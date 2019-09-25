cd05ab7e7b26804c3a20cbe69974c4dcfa8a2481
John Mateer <jj.mateer@live.com>
2019-09-24 19:50:13 -0700

    Added html to handlebars and linked css. In server, required express-handlebars, set public as static folder, set default engine to handlebars and used main as the default layout, and added transitions directly into main.handlebars for them to work. As of now, pages render html and css correctly, but I put the app.get directly in server and commented out the entire apiRoutes file.

---
+++ routes/apiRoutes.js
@@ -0,0 +1,8 @@
+// Routes
+// module.exports = function (app) {
+//     app.get("/", function (req, res) {
+//         socialNetwork.findAll({}).then(function (result) {
+//             res.json(result)
+//         });
+//     });
+// }
