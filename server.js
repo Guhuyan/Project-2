e0df3e41e5f9062f9d2fc9618a680c2ee5e00642
John Mateer <jj.mateer@live.com>
2019-09-25 19:27:47 -0700

    Updated index.handlebars and added login.handlebars.

--- server.js
+++ server.js
@@ -10,8 +10,11 @@ app.use(express.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-app.get('/',function(req,res) {
+app.get('/', function (req, res) {
   res.render('index');
+});
+app.get('/login', function (req, res) {
+  res.render('login');
 });
 
 app.listen(PORT, function () {
