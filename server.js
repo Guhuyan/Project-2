ae6b75817d5440b50c1d9632b06b11df40a2c15e
Gue <guehyang@gmail.com>
2019-09-25 20:11:51 -0700

    Reorganize blocks of code in server.js, added models as a dependency, added html and api routes, and sync the server with the database.

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
-const mysql = require('mysql')
-const exphbs = require("express-handlebars");
-const PORT = process.env.PORT || 8080
+const mysql = require("mysql");
 const express = require("express");
-var app = express();
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.use(express.static("public"));
+// Requiring our models for syncing
+const db = require("./models");
+
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Static directory
+app.use(express.static("public"));
+
+// Handlebars
+const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-app.get('/', function (req, res) {
-  res.render('index');
-});
-app.get('/login', function (req, res) {
-  res.render('login');
-});
 
-app.listen(PORT, function () {
-  console.log("Server listening on: http://localhost:" + PORT);
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./routes/htmlRoutes.js")(app);
+require("./routes/apiRoutes.js")(app);
+
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
